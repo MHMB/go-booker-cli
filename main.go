bbc5fafcce7ed161e8f39148892c5f9353d2b076
Mohammad Hosein Moti Birjandi <m.h.motie@gmail.com>
2024-09-27 14:31:13 +0330

    feat: using slices

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func main() {
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and ", remainingTickets, "are still available!")
-	fmt.Println("Get your tickets here to attend")	
+	fmt.Println("Get your tickets here to attend")
 
-	var bookings [50]string
+	var bookings []string
 
 	var firstName string
 	var lastName string
@@ -33,7 +33,7 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - uint(userTickets)
-	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
