0ab015e9200bf1263e82bdd964e12d8997e88f64
Mohammad Hosein Moti Birjandi <m.h.motie@gmail.com>
2024-09-27 14:09:44 +0330

    feat: different syntaxes of defining variables

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and ", remainingTickets, "are still available!")
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
